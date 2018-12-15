c25ff4d71d53f5e91a76ccfb06f944dedaf48e96
Revinant <aleksandrpavlovic969@gmail.com>
2018-12-15 13:42:22 +0300

    Для сдачи дз

--- js3/taskNumberFour.js
+++ js3/taskNumberFour.js
@@ -1,9 +1,8 @@
 "use strict";
 
-nextNumber:
 for (let d = 2; d <= 100; d++) {
     for (let b = 2; b < d; b++) {
-        if (d % b === 0) continue nextNumber;
+        if (d % b === 0) ;
     }
         console.log( d );
     }
